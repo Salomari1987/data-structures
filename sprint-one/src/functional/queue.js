d03be93494980142de50dab02b48efe45671c50d
Saif Elokour <saiffelokour@gmail.com>
2016-06-06 11:37:16 +0300

    Instantiate Stack & Queue

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,28 @@ var Queue = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
-
+  someInstance.storage = storage  
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    storage[someInstance.size()] = value
   };
 
   someInstance.dequeue = function() {
+    var item = storage[0]
+    for(var i = 0; i < someInstance.size() - 1; i++){
+      storage[i] = storage[i + 1]
+    }
+    delete storage[someInstance.size() - 1]
+    return item
   };
 
   someInstance.size = function() {
+    var count = 0;
+    for(var key in storage){
+      count++;
+    }
+    return count;
   };
 
   return someInstance;
