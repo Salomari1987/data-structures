69dcd8ec25fc402ba47b44a2017a8a71bcd69f7c
Saif Elokour <saiffelokour@gmail.com>
2016-06-06 16:03:46 +0300

    Implement Stack & Queue

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,24 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.storage = {}
 };
 
+Stack.prototype.push = function(value) {
+	this.storage[this.size()] = value
+};
+
+Stack.prototype.pop = function() {
+	var item = this.storage[this.size() - 1]
+	delete this.storage[this.size() - 1]
+	return item
+};
+
+Stack.prototype.size = function() {
+	var count = 0;
+	for(var key in this.storage){
+		count++;
+	}
+	return count;
+}
 
