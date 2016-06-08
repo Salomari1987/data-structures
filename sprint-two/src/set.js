2bbd80c264abd320e15d582f79e41f544891d57c
Saif Elokour <saiffelokour@gmail.com>
2016-06-08 09:33:50 +0300

    Implement Set Function

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -7,12 +7,22 @@ var Set = function() {
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+	if(this._storage === undefined){
+		this._storage = []
+	}
+	if(!this.contains(item)){
+		this._storage.push(item)
+	}
 };
 
 setPrototype.contains = function(item) {
+	return _.reduce(this._storage, (acc, element) => {item === element ? acc = true : 0; return acc}, false)
 };
 
 setPrototype.remove = function(item) {
+	if(this.contains(item)){
+		this._storage.splice(_.indexOf(item), 1)
+	}
 };
 
 /*
